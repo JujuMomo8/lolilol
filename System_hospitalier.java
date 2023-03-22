408ab53b92bc43f75bbd2ae801737560642d100b
Julien <jujumrll8@gmail.com>
2023-03-22 16:48:14 +0100

    ajout de commentaire sur les methode des deux fonction demandées

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -36,11 +36,13 @@ public class System_hospitalier {
 	private String analyserRequete(Requête requête) {
 		// Analyser la requête
 		return "requête analysée";
+		//retourne "requête analysée"
 	}
 	
 	private String comparerDonnes() {
 		// Comparaison des données
 		return "Cluster trouvé";
+		//retourne "Cluster trouvé"
 	}
 
 }
